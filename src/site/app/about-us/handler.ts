a1f07b217359884e8a019dbb446aaa3f94bf529f
Daniel Olawoyin <olawoyindaniel95@gmail.com>
2026-02-16 03:21:08 +0100

    feat: set up initial site structure, core components, CMS types, and About Us page with handlers.

--- src/site/app/about-us/handler.ts
+++ src/site/app/about-us/handler.ts
@@ -3,11 +3,15 @@ import { getAll } from '../../../types/cms-types';
 
 export default async function handler(req: Request, res: Response)
 {
-    const volunteers = await getAll('people', { limit: 10 });
+    const [ volunteers, reviews ] = await Promise.all([
+        getAll('people', { limit: 10 }),
+        getAll('reviews', { limit: 10 })
+    ]);
 
     return {
         data: {
-            volunteers: volunteers.success ? volunteers.data.entries : []
+            volunteers: volunteers.success ? volunteers.data.entries : [],
+            reviews: reviews.success ? reviews.data.entries : []
         },
         metadata: {
             title: 'About Us - Cheriven Foundation',
